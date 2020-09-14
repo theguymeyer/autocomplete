bb3b3acab682820e3ca9ce85af3c2a8f5962b025
Guy Meyer <guymeyer1995@gmail.com>
2020-09-14 12:18:01 -0400

    move autocomp location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <AutoComp />
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
